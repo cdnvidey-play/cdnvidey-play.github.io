e4d5a168a7ec85944c41eb09b161d751c2fcb66f
cdnvidey-play <Cinakw36@gmail.com>
2026-07-20 13:22:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11341122
+    "zoneId": 10861423
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
